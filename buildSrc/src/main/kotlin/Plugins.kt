1026c2039311840d9a56a53e18b06aa0d9f757dd
Luca Spinazzola <mspinluca@gmail.com>
2022-06-08 18:04:11 -0400

    fix build. update to 1.6.21, update dependencies

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -10,10 +10,10 @@ inline val PluginDependenciesSpec.multiplatform: PluginDependencySpec
     get() = kotlin("multiplatform")
 
 inline val PluginDependenciesSpec.`nexus-staging`: PluginDependencySpec
-    get() = id("io.codearte.nexus-staging") version "0.22.0"
+    get() = id("io.codearte.nexus-staging") version "0.30.0"
 
 inline val PluginDependenciesSpec.dokka: PluginDependencySpec
-    get() = id("org.jetbrains.dokka") version "1.4.20"
+    get() = id("org.jetbrains.dokka") version "1.6.21"
 
 inline val PluginDependenciesSpec.`android-library`: PluginDependencySpec
     get() = id("com.android.library")
